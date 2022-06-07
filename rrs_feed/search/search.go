63e2d4ee0de776704de6c5b43c53f808e122e5a4
Siwei Wang <wangsiwei2003@hotmail.com>
2022-06-06 22:57:42 -0400

    add rrs feed

--- rrs_feed/search/search.go
+++ rrs_feed/search/search.go
@@ -9,7 +9,7 @@ import (
 var matchers = make(map[string]Matcher)
 
 // Run performs the search logic
-func Run(seachTerm string) {
+func Run(searchTerm string) {
 	//Retrieve the list of feeds to search through
 	feeds, err := RetrieveFeeds()
 	if err != nil {
@@ -27,6 +27,7 @@ func Run(seachTerm string) {
 
 	// Launch a goroutine for each feed to find the results
 	for _, feed := range feeds {
+		matcher, exists := matchers[feed.Type]
 		// Retrieve a matcher for the search
 		if !exists {
 			matcher = matchers["default"]
@@ -34,7 +35,7 @@ func Run(seachTerm string) {
 
 		// Launch the goroutine to perform the search
 		go func(matcher Matcher, feed *Feed) {
-			Match(match, feed, searchTerm, results)
+			Match(matcher, feed, searchTerm, results)
 			waitGroup.Done()
 		}(matcher, feed)
 
@@ -43,7 +44,7 @@ func Run(seachTerm string) {
 	// Launch a goroutine to monitor when all the work is done
 	go func() {
 		// Wait for everything to be processed
-		waitGroup.wait()
+		waitGroup.Wait()
 
 		// Close the channel to signal to the Display
 		// function that we can exit the program
@@ -57,10 +58,10 @@ func Run(seachTerm string) {
 
 // Register is called to register a matcher for use by the program
 func Register(feedType string, matcher Matcher) {
-	if _, exists := matcher[feedType]; exists {
+	if _, exists := matchers[feedType]; exists {
 		log.Fatalln(feedType, "Matcher already registered")
 	}
 
-	log.Println("Register", feedtype, "matcher")
+	log.Println("Register", feedType, "matcher")
 	matchers[feedType] = matcher
 }
