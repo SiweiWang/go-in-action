2087b3ae0ee43fad294075cd7b4fff334f4dc116
Siwei Wang <wangsiwei2003@hotmail.com>
2022-07-03 11:30:32 -0400

    add main for runner

---
+++ runner/go.mod
@@ -0,0 +1,3 @@
+module github.com/SiweiWang/go-in-action/runner
+
+go 1.18
--- runner/main/main.go
+++ runner/main/main.go
@@ -2,3 +2,46 @@
 // monitor the amount of time the program is running and terminate
 // the program if it runs too long.
 package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/SiweiWang/go-in-action/runner"
+)
+
+const timeout = 5 * time.Second
+
+func main() {
+	log.Println("Starting work.")
+
+	r := runner.New(timeout)
+
+	r.Add(createTask(), createTask(), createTask(), createTask(), createTask())
+
+	if err := r.Start(); err != nil {
+		switch err {
+		case runner.ErrTimeout:
+			log.Println("Terminating due to timeout.")
+			os.Exit(1)
+
+		case runner.ErrInterrupt:
+			log.Println("Terminating due to interrupt.")
+			os.Exit(1)
+		}
+	}
+
+	log.Println("Process ended.")
+}
+
+// createTask returns an example task that sleeps for the specified
+// number of seconds based on the id.
+
+func createTask() func(int) {
+	return func(id int) {
+		log.Printf("Processor - Task #%d.", id)
+		log.Printf("time to sleep %d seconda.", time.Duration(id))
+		time.Sleep(time.Duration(id) * time.Second)
+	}
+}
